7432037bf38e03d33fb68faa3d03aa78720bef2a
Drew McMahon <andrew.mcmahon@yale.edu>
2015-02-10 22:17:16 +0000

    sendfile deprecated -> sendFile

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-var contextData = {};
 
 app.get('/', function(request, response){
-    response.render('report.html', contextData);
+    response.sendFile('report.html');
 });
 
 app.listen(process.env.PORT || 4000);
